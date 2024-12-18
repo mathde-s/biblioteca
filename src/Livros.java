6619600618f782d8258c47fbbf5104065915059a
matheus <matheus.pereira@zup.com.br>
2024-12-18 09:50:46 -0300

    feat: criação do construtor

--- src/Livros.java
+++ src/Livros.java
@@ -1,6 +1,13 @@
 public class Livros {
-    String titulo;
-    String autor;
-    String isbn;
-    boolean disponivel;
+    private String titulo;
+    private String autor;
+    private String isbn;
+    private boolean disponivel;
+
+    public Livros(String titulo, String autor, String isbn, boolean disponivel){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.isbn = isbn;
+        this.disponivel = disponivel;
+    }
 }
